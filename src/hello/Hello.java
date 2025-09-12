0e6f281c2cd52a864f8dd6cc71cbec594f7625f4
Kaushik <boddula.kaushik@gmail.com>
2025-09-12 13:47:09 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("Hey there I am learning CI");
+    System.out.println("Added WebHooks and its working");
   }
 }
